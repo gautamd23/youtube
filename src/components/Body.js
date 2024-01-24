6fe73f43763d5c94ce0ff0415c08ffab21be68bb
gautamd23 <86918819+gautamd23@users.noreply.github.com>
2024-01-24 15:17:16 +0530

    initial structure of app

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideBar from './SideBar'
+import VideoContainerBody from './VideoContainerBody'
+
+
+export default function Body() {
+  return (
+    <div>
+        <SideBar/>
+        <VideoContainerBody/>
+    </div>
+  )
+}
---
+++ src/components/VideoContainerBody.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ButtonList from './ButtonList'
+import VideoContainer from './VideoContainer'
+
+export default function VideoContainerBody() {
+  return (
+    <div>
+        <ButtonList/>
+        <VideoContainer/>
+    </div>
+  )
+}
