6795f0b445670c02eebfcaaa8c1a4ead01beb827
gautamd23 <86918819+gautamd23@users.noreply.github.com>
2024-01-28 12:54:46 +0530

    created routers

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import { Provider } from "react-redux";
 import Body from "./components/Body";
 import Head from "./components/Head";
 import appStore from "./utils/appStore";
+import { BrowserRouter, RouterProvider, createBrowserRouter } from "react-router-dom";
+import VideoContainer from "./components/VideoContainer";
+import VideoContainerBody from "./components/VideoContainerBody";
+import WatchPage from "./components/WatchPage";
 
 function App() {
+
+  const appRouter = createBrowserRouter([{
+    path:"/",
+    element:<Body/>,
+    children:[
+      {
+        path:"/",
+        element:<VideoContainerBody/>
+      },
+      {
+        path:"/watch",
+        element:<WatchPage/>
+      }
+    ]
+  }])
   return (
     <Provider store={appStore}>
       <div className="App">
       <Head/>
-      <Body/>
+      <RouterProvider router={appRouter}/>
     </div>
     </Provider>
     
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import SideBar from './SideBar'
 import VideoContainerBody from './VideoContainerBody'
+import { Outlet } from 'react-router-dom'
 
 
 export default function Body() {
@@ -8,7 +9,8 @@ export default function Body() {
     <div className='flex justify-normal w-screen mt-1'>
     
         <SideBar/>
-        <VideoContainerBody/>
+        {/* <VideoContainerBody/> */}
+        <Outlet/>
     </div>
   )
 }
