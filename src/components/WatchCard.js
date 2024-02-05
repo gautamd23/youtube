27b7e2ec5e7968ccc6e089d9cb89ba43e058ff43
gautamd23 <86918819+gautamd23@users.noreply.github.com>
2024-02-05 13:35:31 +0530

    added like and dislike btn

--- src/components/WatchCard.js
+++ src/components/WatchCard.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import like from '../Assets/like.png';
+import dislike from '../Assets/dislike.jpg'
 
 export default function WatchCard({ searchParams }) {
   const watchVideo = useSelector((store) => store?.video?.watch);
-  console.log(watchVideo);
-  const { snippet,channelTitle } = watchVideo;
-  console.log(snippet)
-    const { title, thumbnails } = snippet;
+  console.log(watchVideo?.snippet);
+//   const { snippet,channelTitle } = watchVideo;
+//   console.log(snippet)
+//     const { title, thumbnails } = snippet;
   return (
     <div className="">
       <div>
@@ -21,8 +23,17 @@ export default function WatchCard({ searchParams }) {
           allowFullScreen
         ></iframe>
       </div>
-      <div className="py-3 font-bold text-xl w-[700px]">{title}</div>
-      <div>{channelTitle}</div>
+      <div className="py-3 font-bold text-xl w-[700px]">{watchVideo?.snippet.title}</div>
+      <div>{watchVideo?.snippet.channelTitle}</div>
+      <div className="flex">
+        <div>
+            <button>Subscribe</button>
+        </div>
+        <div>
+            <button className="px-5 border-2 rounded-l-full bg-gray-200 " ><img className="w-7" src={like}/></button>
+            <button className="px-5 rotate-180 border-2 rounded-l-full bg-gray-200"><img className="w-7" src={like}/></button>
+        </div>
+      </div>
     </div>
   );
 }
